a78b373d34395dc5ffc1db4302b7f0a300f72958
diop <fodesdiop@gmail.com>
2017-08-21 18:34:14 -0700

    use conventional naming for css

--- part-3/modal.js
+++ part-3/modal.js
@@ -1,19 +1,19 @@
 
 const cartButton = document.getElementById("cart-button")
-const modal = document.getElementById(".modal")
-const modalContent = document.querySelector(".modal-content")
+const modalOverlay = document.getElementById(".modal-overlay")
+const modal = document.querySelector(".modal-container")
 const cartCounter = document.getElementById("cart-item-count")
 const closeButton = document.querySelector(".cart-close")
 const clearCartButton = document.querySelector(".modal-footer button")
 
 function openModal() {
+	modalOverlay.classList.add("show-modal")
 	modal.classList.add("show-modal")
-	modalContent.classList.add("show-modal")
 }
 
 function closeModal() {
+  modalOverlay.classList.remove("show-modal")
   modal.classList.remove("show-modal")
-  modalContent.classList.remove("show-modal")
 }
 
 cartButton.addEventListener("click", openModal)
