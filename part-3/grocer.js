839ac7e3d7420f5530fbd6329c2609157dc81e0d
diop <fodesdiop@gmail.com>
2017-08-21 22:52:03 -0700

    implement add items to modal

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -5,6 +5,8 @@ const modal = document.querySelector(".modal-container")
 const cartCounter = document.getElementById("cart-item-count")
 const closeButton = document.querySelector(".cart-close")
 const clearCartButton = document.querySelector(".modal-footer button")
+let itemsArray = []
+let totalPrice
 
 function openModal() {
 	modalOverlay.classList.add("show-modal")
@@ -14,6 +16,34 @@ function openModal() {
 function closeModal() {
   modalOverlay.classList.remove("show-modal")
   modal.classList.remove("show-modal")
+}
+
+function getItemData(element){
+	const modalItemsRow = document.createElement("DIV")
+	const itemNameNode = document.createElement("SPAN")
+	const itemPriceNode = document.createElement("SPAN")
+	const showTotalPrice = document.querySelector(".modal-total-price")
+	const itemPriceAmtNode = document.createTextNode(price)
+	const itemNameTextNode = document.createTextNode(name)
+
+	let childNodes = element.target.parentElement.childNodes
+	let name = childNodes[1].innerHTML
+	let price = childNodes[3].innerHTML
+	itemsArray.push ({ name, price })
+	cartCounter.innerHTML = itemsArray.length
+	getTotalPrice()
+	showTotalPrice.innerHTML = "$" + totalPrice.toFixed(2)
+
+	itemNameNode.appendChild(itemNameTextNode)
+  	modalItemsRow.appendChild(itemNameNode)
+  	itemPriceNode.appendChild(itemPriceAmtNode)
+  	modalItemsRow.appendChild(itemPriceNode)
+  	modalItems.appendChild(modalItemsRow)
+}
+
+function getTotalPrice() {
+  totalPrice = itemsArray.reduce(getSum, 0)
+  return totalPrice
 }
 
 cartButton.addEventListener("click", openModal)
