cf9e12b7eed5f99db2e7d2eeeced456461e0cbe8
diop <fodesdiop@gmail.com>
2017-08-21 17:35:35 -0700

    implement openModal

--- part-3/modal.js
+++ part-3/modal.js
@@ -1,26 +1,11 @@
-// Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("cart-button");
+const cartButton = document.getElementById("cart-button")
+const modal = document.getElementById(".modal")
+const modalContent = document.querySelector(".modal-content")
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+function openModal() {
+	modal.classList.add("show-modal")
+	modalContent.classList.add("show-modal")
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-Try it Yourself »
+cartButton.addEventListener("click", openModal)
