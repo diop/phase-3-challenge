723a1aadbe61c46933b10ae9268046d120339bce
diop <fodesdiop@gmail.com>
2017-08-21 18:13:27 -0700

    implement closeModal

--- part-3/modal.js
+++ part-3/modal.js
@@ -2,10 +2,19 @@
 const cartButton = document.getElementById("cart-button")
 const modal = document.getElementById(".modal")
 const modalContent = document.querySelector(".modal-content")
+const cartCounter = document.getElementById("cart-item-count")
+const closeButton = document.querySelector(".cart-close")
+const clearCartButton = document.querySelector(".modal-footer button")
 
 function openModal() {
 	modal.classList.add("show-modal")
 	modalContent.classList.add("show-modal")
 }
 
+function closeModal() {
+  modal.classList.remove("show-modal")
+  modalContent.classList.remove("show-modal")
+}
+
 cartButton.addEventListener("click", openModal)
+closeButton.addEventListener("click", closeModal)
