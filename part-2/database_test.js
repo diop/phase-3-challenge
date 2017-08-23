9d5ac9ba145a4c0b1bfb0480c869cac13e091740
diop <fodesdiop@gmail.com>
2017-08-23 10:22:31 -0700

    remove extraneous semicolons

--- part-2/database_test.js
+++ part-2/database_test.js
@@ -5,8 +5,8 @@ describe('productList', () => {
   it('returns an array', done => {
     db.productList('dairy')
       .then((list) => {
-        expect(list).to.be.a('Array');
-        done();
+        expect(list).to.be.a('Array')
+        done()
       })
       .catch(console.log);
   })
@@ -16,7 +16,7 @@ describe('shopperOrders', () => {
   it('returns an array', done => {
     db.shopperOrders('3')
       .then((list) => {
-        expect(list).to.be.a('Array');
+        expect(list).to.be.a('Array')
         done();
       })
       .catch(console.log);
@@ -27,9 +27,9 @@ describe('realShoppers', () => {
   it('returns an array', done => {
     db.realShoppers()
       .then((list) => {
-        expect(list).to.be.a('Array');
+        expect(list).to.be.a('Array')
         done();
       })
-      .catch(console.log);
+      .catch(console.log)
   })
 })
