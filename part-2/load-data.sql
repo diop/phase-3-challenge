d8e086fac76415076c13446e9ad4abfe0bf67aa3
diop <fodesdiop@gmail.com>
2017-08-23 00:54:08 -0700

    update load-data

--- part-2/load-data.sql
+++ part-2/load-data.sql
@@ -1,77 +1,16 @@
 \copy grocery_items(name, price, section) FROM './grocery.csv' DELIMITER ',' CSV HEADER;
 
-INSERT INTO shoppers (first, last, email)
-    VALUES ('firstName', 'lastName', 'first@last.com');
-INSERT INTO shoppers (first, last, email)
-    VALUES ('firstName', 'lastName', 'first@last.com');
-INSERT INTO shoppers (first, last, email)
-    VALUES ('firstName', 'lastName', 'first@last.com');
-INSERT INTO shoppers (first, last, email)
-    VALUES ('firstName', 'lastName', 'first@last.com');
-INSERT INTO shoppers (first, last, email)
-    VALUES ('firstName', 'lastName', 'first@last.com');
-INSERT INTO shoppers (first, last, email)
-    VALUES ('firstName', 'lastName', 'first@last.com');
+INSERT INTO shoppers (name)
+VALUES ('Joe'), ('James'), ('John'), ('Jimbo'), ('Jim');
 
-
-INSERT INTO orders (shopper_id)
-    VALUES (1);
-INSERT INTO orders (shopper_id)
-    VALUES (2);
-INSERT INTO orders (shopper_id)
-    VALUES (3);
-INSERT INTO orders (shopper_id)
-    VALUES (4);
-INSERT INTO orders (shopper_id)
-    VALUES (5);
-INSERT INTO orders (shopper_id)
-    VALUES (1);
-INSERT INTO orders (shopper_id)
-    VALUES (2);
-INSERT INTO orders (shopper_id)
-    VALUES (3);
-INSERT INTO orders (shopper_id)
-    VALUES (4);
-INSERT INTO orders (shopper_id)
-    VALUES (5);
-
-INSERT INTO order_items (id, item_id)
-    VALUES (1, 31);
-INSERT INTO order_items (id, item_id)
-    VALUES (1, 12);
-INSERT INTO order_items (id, item_id)
-    VALUES (2, 6);
-INSERT INTO order_items (id, item_id)
-    VALUES (2, 14);
-INSERT INTO order_items (id, item_id)
-    VALUES (3, 5);
-INSERT INTO order_items (id, item_id)
-    VALUES (3, 2);
-INSERT INTO order_items (id, item_id)
-    VALUES (4, 32);
-INSERT INTO order_items (id, item_id)
-    VALUES (4, 18);
-INSERT INTO order_items (id, item_id)
-    VALUES (5, 17);
-INSERT INTO order_items (id, item_id)
-    VALUES (5, 9);
-INSERT INTO order_items (id, item_id)
-    VALUES (6, 14);
-INSERT INTO order_items (id, item_id)
-    VALUES (6, 27);
-INSERT INTO order_items (id, item_id)
-    VALUES (7, 15);
-INSERT INTO order_items (id, item_id)
-    VALUES (7, 1);
-INSERT INTO order_items (id, item_id)
-    VALUES (8, 32);
-INSERT INTO order_items (id, item_id)
-    VALUES (8, 25);
-INSERT INTO order_items (id, item_id)
-    VALUES (9, 11);
-INSERT INTO order_items (id, item_id)
-    VALUES (9, 16);
-INSERT INTO order_items (id, item_id)
-    VALUES (10, 19);
-INSERT INTO order_items (id, item_id)
-    VALUES (10, 29);
+INSERT INTO orders (id, item_id, shopper_id)
+VALUES
+  (1, 2, 3),
+  (1, 3, 3),
+  (1, 4, 3),
+  (2, 1, 3),
+  (2, 2, 3),
+  (3, 6, 4),
+  (4, 10, 1),
+  (5, 9, 5),
+  (5, 17, 5);
