aab9d12c1150a0eefdd7ca8008916fe56285debe
diop <fodesdiop@gmail.com>
2017-08-23 01:44:23 -0700

    to do: fix connection

--- part-2/connection.js
+++ part-2/connection.js
@@ -1,5 +1,8 @@
-const pgp = require('pg-promise')();
-const connectionString = 'postgres://localhost:5432/grocery_store'
-const db = pgp(connectionString)
+const connection = {
+  const pgp = require('pg-promise')();
+  const connectionString = 'postgres://localhost:5432/grocery_store'
+  const db = pgp(connectionString)
+
+}
 
 module.exports = connection
