22acf600a4dbe61c776dbc2ea20537488caf7c6e
diop <fodesdiop@gmail.com>
2017-08-23 11:21:48 -0700

    refactor routes

--- part-1/api/routes.js
+++ part-1/api/routes.js
@@ -1,24 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const daysOfWeek = { monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7 }
 
 
 router.get('/days/:day', (request, response) => {
-
   let day = request.params.day.toLowerCase()
-  let daysOfWeek = { monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7 }
-  !daysOfWeek[day] ? response.status(400).send(`response: '${day}' is not a valid day!`) :
-  response.status(200).set('Content-Type', 'text/plain').send(`response: ${daysOfWeek[day]}`)
-
+  !daysOfWeek[day] ?
+    response.status(400).send(`response: '${day}' is not a valid day!`) :
+    response.status(200)
+      .set('Content-Type', 'text/plain')
+      .send(`response: ${daysOfWeek[day]}`)
 })
 
-router.post('/array/concat', (request, response) => {
-
+router.post('/array/concat', (request, response, next) => {
   let array1 = request.body.array1
   let array2 = request.body.array2
-  let result = array1.concat(array2)
-
+  let concat = array1.concat(array2)
   Array.isArray(array1) && Array.isArray(array2) ?
-  response.header('content-type', 'application/json').json( {"result": result} ) :
+  response.header('content-type', 'application/json').json( {"result": concat} ) :
   response.status(400)
     .header('content-type', 'application/json')
     .json( {"error": "Input data should be of type Array."} )
--- part-1/app.js
+++ part-1/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
+const api = require('./api/routes')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
-const bodyParser = bodyParser.json()
 
 app.use(bodyParser())
 app.use(bodyParser.urlencoded({ extended: false }))
