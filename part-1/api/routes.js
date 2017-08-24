26fe3e46737fd614ad3ef267435335f867c4a7c3
diop <fodesdiop@gmail.com>
2017-08-24 09:13:08 -0700

    refactor routes

--- part-1/api/routes.js
+++ part-1/api/routes.js
@@ -5,22 +5,29 @@ const daysOfWeek = { monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5
 
 router.get('/days/:day', (request, response) => {
   let day = request.params.day.toLowerCase()
-  !daysOfWeek[day] ?
-    response.status(400).send(`response: '${day}' is not a valid day!`) :
+  
+  if (daysOfWeek[day]){
+      response.status(400)
+      .send(`response: '${day}' is not a valid day!`)
+  } else {
     response.status(200)
       .set('Content-Type', 'text/plain')
       .send(`response: ${daysOfWeek[day]}`)
+  }
 })
 
 router.post('/array/concat', (request, response, next) => {
   let array1 = request.body.array1
   let array2 = request.body.array2
   let concat = array1.concat(array2)
-  Array.isArray(array1) && Array.isArray(array2) ?
-  response.header('content-type', 'application/json').json( {"result": concat} ) :
-  response.status(400)
-    .header('content-type', 'application/json')
-    .json( {"error": "Input data should be of type Array."} )
+
+  if (Array.isArray(array1) && Array.isArray(array2)){
+    response.header('content-type', 'application/json').json( {"result": concat} )
+  } elese {
+    response.status(400)
+      .header('content-type', 'application/json')
+      .json( {"error": "Input data should be of type Array."} )
+  }
 })
 
 module.exports = router
